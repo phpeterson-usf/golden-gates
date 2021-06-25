b05f20393bdbe86ce178717bed141721081d0d0b
Phil Peterson <phpeterson@usfca.edu>
2021-06-24 20:33:30 -0700

    drag/grid

---
+++ client/src/components/other/Wire.vue
@@ -0,0 +1,21 @@
+<template>
+    <line 
+        v-for="(seg, index) in item.segments"
+        :key="index"
+        :x1="seg.x1Pix"
+        :y1="seg.y1Pix"
+        :x2="seg.x2Pix"
+        :y2="seg.y2Pix"
+    />
+
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: "Wire",
+    props: {
+        item: Object,
+    },
+  })
+</script>
